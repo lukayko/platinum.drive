3852c234f3151db931b8d44add602c13852cb94c
lukayko <lukay.cervenka@gmail.com>
2023-05-28 23:09:21 +0200

    updated our services page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,5 +1,64 @@
 const About = () => {
-  return <div className="text-bold mt-16"></div>;
+  return (
+    <div className="mt-24 w-[90%] mx-auto">
+      <section>
+        <h1 className="font-bold text-2xl text-tertiary">
+          About platinum<span className="text-secondary">.drive</span>
+        </h1>
+        <p>
+          For the past 10 years, our company has been a leader in the luxury
+          transportation industry, catering to discerning clients in Bratislava
+          and beyond. Throughout our remarkable journey, we have achieved
+          numerous milestones and experienced remarkable successes that have
+          solidified our position as a premier service provider.
+        </p>
+        <p>
+          Since our inception, our commitment to excellence has driven us to
+          deliver unparalleled customer satisfaction. We take pride in our
+          ability to match and exceed the expectations of our clients, ensuring
+          that every interaction with our company is met with professionalism,
+          attention to detail, and a personalized touch.
+        </p>
+        <p>
+          Over the years, we have built a reputation for providing top-notch
+          chauffeur services that offer the highest level of luxury, comfort,
+          and reliability. Our team of professional drivers undergoes rigorous
+          training, embodying the values of our company and exemplifying
+          exceptional service. We have received accolades from our clients for
+          our punctuality, immaculate fleet, and the seamless experiences we
+          provide.
+        </p>
+        <p>
+          One of our notable achievements has been our successful expansion into
+          car rental services. Recognizing the evolving needs of our clientele,
+          we have curated a distinguished fleet of premium vehicles, offering an
+          unmatched selection for those seeking style, performance, and
+          sophistication. Our dedication to maintaining our fleet's impeccable
+          condition has earned us the trust and loyalty of our customers.
+        </p>
+        <p>
+          We are also proud to have developed a strong network of global
+          partners, enabling us to offer seamless transportation services across
+          borders. This network, combined with our expertise in logistics and
+          documentation, allows us to facilitate car transportation between
+          countries efficiently and securely. Our ability to navigate complex
+          international regulations and deliver vehicles safely has garnered
+          acclaim from clients who entrust us with their prized possessions.
+        </p>
+        <p>
+          As we reflect on our 10-year history, we are grateful for the
+          relationships we have formed with our clients. Their trust and support
+          have been instrumental in our success. Looking to the future, we
+          remain dedicated to continuously enhancing our services, embracing
+          innovation, and embracing the unusual to ensure the complete
+          satisfaction of our valued customers. With our unwavering commitment
+          to excellence and a decade of accomplishments behind us, we stand
+          poised to create even more remarkable experiences in the years to
+          come.
+        </p>
+      </section>
+    </div>
+  );
 };
 
 export default About;
